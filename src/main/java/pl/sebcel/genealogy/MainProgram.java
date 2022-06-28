46889ec941efd83fd1d3faea18120bbad112e96c
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2022-06-28 17:55:09 +0200

    Updating log4j

--- src/main/java/pl/sebcel/genealogy/MainProgram.java
+++ src/main/java/pl/sebcel/genealogy/MainProgram.java
@@ -1,12 +1,17 @@
 package pl.sebcel.genealogy;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import pl.sebcel.genealogy.gui.MainFrame;
 
 public class MainProgram {
+	
+	private static Logger log = LogManager.getLogger(MainProgram.class); 
 
 	public static void main(String[] args) {
+		log.info("Starting Genealogy");
 		MainFrame mainFrame = new MainFrame();
 		mainFrame.setVisible(true);
 	}
-
 }
